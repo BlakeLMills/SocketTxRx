aa544acfc7bff0a941c4391fc1da17f17eabe91b
Blake L. Mills <blam@pop-os.localdomain>
2023-08-27 17:46:31 -0600

    Adding pre-commits and poetry project organization

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SocketTxRx
+
+## Setting up the Python Environment
+1. Use Poetry along with Python 3.11
+2. Issue command: ``` poetry config virtualenvs.in-project true ```
+3. Issue command: ``` poetry install --sync ```
+4. Issue command: ``` poetry shell ``` or ``` source .venv/bin/activate ```
--- SocketTxRx/SocketTxRx.py
+++ SocketTxRx/SocketTxRx.py
@@ -1,45 +1,67 @@
+import argparse  # to get cmd line args
 import socket
-import argparse # to get cmd line args
 import time
 from multiprocessing import Process
 
+
 def getArguments():
-    parser = argparse.ArgumentParser(description='Send and Receive multicast.')
-    parser.add_argument('--rx-ip', dest='RxIp', default='', type=str, help='IP Address to receive packets')
-    parser.add_argument('--tx-ip', dest='TxIp', default='', type=str, help='IP Address to send packets')
-    parser.add_argument('--rx-port', dest='RxPort', default='', type=int, help='Port to receive packets')
-    parser.add_argument('--tx-port', dest='TxPort', default='', type=int, help='Port to send packets')
-    parser.add_argument('--script-timeout', dest='timeout', default='5', type=int, help='Script elapse time in seconds before it will exit')
+    parser = argparse.ArgumentParser(description="Send and Receive multicast.")
+    parser.add_argument(
+        "--rx-ip",
+        dest="RxIp",
+        default="",
+        type=str,
+        help="IP Address to receive packets",
+    )
+    parser.add_argument(
+        "--tx-ip", dest="TxIp", default="", type=str, help="IP Address to send packets"
+    )
+    parser.add_argument(
+        "--rx-port", dest="RxPort", default="", type=int, help="Port to receive packets"
+    )
+    parser.add_argument(
+        "--tx-port", dest="TxPort", default="", type=int, help="Port to send packets"
+    )
+    parser.add_argument(
+        "--script-timeout",
+        dest="timeout",
+        default="5",
+        type=int,
+        help="Script elapse time in seconds before it will exit",
+    )
 
 
 def openMulticastUdpTx(TxIp, TxPort):
-
     # init the socket
-    TransmitSock = socket.socket(socket.AF_INET, # Internet
-                                socket.SOCK_DGRAM) # UDP
+    TransmitSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
     print("Sending packets to IP:       {}".format(TxIP))
     print("Sending packets to UDP Port: {}".format(TxPort))
     return TransmitSock
 
+
 def openMulticastUdpRx(RxIp, RxPort):
-    ReceiveSock = socket.socket(socket.AF_INET, # Internet
-                                socket.SOCK_DGRAM) # UDP
+    ReceiveSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
     try:
-        ReceiveSock.setsockopt(socket.SOL_SOCKET,
-                                socket.SOCK_DGRAM)
-    except AttributError:
-        pass
+        ReceiveSock.setsockopt(socket.SOL_SOCKET, socket.SOCK_DGRAM)
+    except AttributeError:
+        suppress(AttributeError)
     ReceiveSock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
     ReceiveSock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
     ReceiveSock.settimeout(10)
-    ReceiveSock.bind(('', RxPort))
+    ReceiveSock.bind(("", RxPort))
     host = socket.gethostbyname(socket.gethostname())
-    ReceiveSock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(host))
-    ReceiveSock.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(RxIP) + socket.inet_aton(host))
+    ReceiveSock.setsockopt(
+        socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(host)
+    )
+    ReceiveSock.setsockopt(
+        socket.SOL_IP,
+        socket.IP_ADD_MEMBERSHIP,
+        socket.inet_aton(RxIP) + socket.inet_aton(host),
+    )
     return ReceiveSock
 
-def ReceiveMessages():
 
+def ReceiveMessages():
     scriptStartTime = time.time()
     args = getArguments()
     ReceiveSock = openMulticastUdpRx(args.RxIp, args.RxPort)
@@ -53,12 +75,12 @@ def ReceiveMessages():
 
 
 def SendMessages():
-
     args = getArguments()
     TransmitSock = openMulticastUdpTx(args.TxIp, args.TxPort)
 
     frame = "Data to send"
     TransmitSock.sendto(fram, (args.TxIp, args.TxPort))
+
 
 def runInParallel(*fns):
     proc = []
@@ -69,11 +91,10 @@ def runInParallel(*fns):
     for p in proc:
         p.join()
 
+
 def main():
     runInParallel(SendMessages, ReceiveMessages)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
